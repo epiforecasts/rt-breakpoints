3436c59a80e8b3b06edcad72bd4f2c57bdb54431
kathsherratt <kathsherratt@gmail.com>
2020-11-17 17:30:09 +0000

    fresh estimates

--- run-rt-breakpoint.R
+++ run-rt-breakpoint.R
@@ -44,8 +44,8 @@ cases_delay <- readRDS(here::here("delays", "data", "public_onset_to_report_dela
 
 
 # get private data --------------------------------------------------------
-# raw <- readRDS("covid19_uk_forecast_data/data/processed/latest_data.rds")
-raw <- readRDS(path.expand(file.path("C:", "Users", "kaths", "Github", "covid19_uk_forecast_data", "data", "processed", "latest_data.rds")))
+raw <- readRDS("~/covid19_uk_forecast_data/data/processed/latest_data.rds")
+# raw <- readRDS(path.expand(file.path("C:", "Users", "kaths", "Github", "covid19_uk_forecast_data", "data", "processed", "latest_data.rds")))
 
 raw$value_desc <- NULL
 data <- raw[raw$type == "Data" ,]
